e1fb9a1ad8bac5cb289e4ee80e96e5bc6fe726ee
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-07-06 15:47:27 -0700

    setup the env

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/ui/button';
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="outline" size="lg">
+        Hello
+      </Button>
     </div>
   );
 }
