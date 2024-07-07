f9ea5266eecd7b52d738aac57cc988eb96d02ab2
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-07-06 19:19:18 -0700

    filter board

--- src/components/JobDash.tsx
+++ src/components/JobDash.tsx
@@ -1,6 +1,14 @@
 import { Button } from '@nextui-org/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 const JobDash = () => {
   return (
@@ -33,24 +41,9 @@ const JobDash = () => {
               </Label>
               <Input type="text" id="title" placeholder="Programming" />
             </div>
-            <div className="w-[90%]">
-              <Label
-                className="text-mytext font-header2 font-bold mb-4"
-                htmlFor="industry"
-              >
-                Email
-              </Label>
-              <Input type="text" id="industry" placeholder="Tech" />
-            </div>
-            <div className="w-[90%]">
-              <Label
-                className="text-mytext text-header2 font-bold mb-4"
-                htmlFor="Type"
-              >
-                Email
-              </Label>
-              <Input type="text" id="Type" placeholder="Full Time" />
-            </div>
+            <SelectLocation required="industry" />
+            <SelectLocation required="type" />
+            <SelectLocation required="location" />
           </div>
         </div>
       </div>
@@ -81,6 +74,96 @@ function SearchBar() {
   );
 }
 
-// function SelectLocation() {
-//   return <div></div>;
-// }
+type LocationProp = {
+  required?: string;
+};
+
+function SelectLocation({ required }: LocationProp) {
+  const inquiry = required;
+
+  if (inquiry === 'location') {
+    return (
+      <div className="">
+        <Label
+          className="text-mytext text-header2 font-bold mb-4"
+          htmlFor="location"
+        >
+          Location
+        </Label>
+        <Select>
+          <SelectTrigger className="w-[90%]">
+            <SelectValue placeholder="Select Location" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup className="text-mytext font-body font-bold">
+              <SelectItem value="anywhere">Anywhere</SelectItem>
+              <SelectItem value="Australia">Australia</SelectItem>
+              <SelectItem value="Brazil">Brazil</SelectItem>
+              <SelectItem value="Cannada">Cannada</SelectItem>
+              <SelectItem value="Europe">Europe</SelectItem>
+              <SelectItem value="UK">UK</SelectItem>
+              <SelectItem value="USA">USA</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+    );
+  }
+
+  if (inquiry === 'type') {
+    return (
+      <div>
+        <Label
+          className="text-mytext text-header2 font-bold mb-4"
+          htmlFor="Type"
+        >
+          Job Type
+        </Label>
+        <Select>
+          <SelectTrigger className="w-[90%]">
+            <SelectValue placeholder="Select Type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup className="text-mytext font-body font-bold">
+              <SelectItem value="Full Time">Full Time</SelectItem>
+              <SelectItem value="Contract">Contract</SelectItem>
+              <SelectItem value="Internship">Internship</SelectItem>
+              <SelectItem value="Part Time">Part Time</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+    );
+  }
+
+  if (inquiry === 'industry') {
+    return (
+      <div>
+        <Label
+          className="text-mytext text-header2 font-bold mb-4"
+          htmlFor="industry"
+        >
+          Industry
+        </Label>
+        <Select>
+          <SelectTrigger className="w-[90%]">
+            <SelectValue placeholder="Industry" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup className="text-mytext font-body font-bold">
+              <SelectItem value="Any Category">Any Category</SelectItem>
+              <SelectItem value="Bussiness Development">
+                Bussiness Development
+              </SelectItem>
+              <SelectItem value="Web and App Design">
+                Web and App Design
+              </SelectItem>
+              <SelectItem value="Sales">Sales</SelectItem>
+              <SelectItem value="Programming">Programming</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+    );
+  }
+}
